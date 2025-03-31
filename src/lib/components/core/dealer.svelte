303acd3dbe4fc571444e23dc02d2c0fcbccdda23
Florian <79457058+florian-alb@users.noreply.github.com>
2025-03-31 14:35:56 +0200

    feat(layout): enhance layout with main wrapper and styling; feat(page): restructure page with game components and layout

---
+++ src/lib/components/core/dealer.svelte
@@ -0,0 +1,15 @@
+<script>
+
+	import Card from "./card.svelte";
+
+</script>
+<div>
+    <h1 class="text-bold text-2xl text-center mb-4">Dealer</h1>
+    <div class="flex items-center gap-4 pl-[calc(40px_+_1rem)]">
+        <div class="flex gap-4">
+            <Card/>
+            <Card back={true}/>
+        </div>
+        <div class="rounded-full p-4 bg-black/20 size-10 flex justify-center items-center font-bold">19</div>
+    </div>
+</div>
